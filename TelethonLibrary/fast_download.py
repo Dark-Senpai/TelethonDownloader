0c3191fe05740e652c3adb3323ede13016f047ae
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-23 16:45:15 +0530

    Update fast_download.py

--- TelethonLibrary/fast_download.py
+++ TelethonLibrary/fast_download.py
@@ -1,2 +1,137 @@
 # @dark-super-me / @dark-senpai 
-########### I hope this could be helpful #############
+
+import asyncio
+import glob
+import math
+import os
+import re
+import sys
+import time
+from traceback import format_exc
+
+from . import *
+
+# --------------------------------------------------------------------- #
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
+
+#_____________________ Fast Download / Fast Upload ____________________#
+
+async def uploader(file, name, taime, event, msg):
+    with open(file, "rb") as f:
+        result = await uploadable(
+            client=event.client,
+            file=f,
+            filename=name,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d,
+                    t,
+                    event,
+                    taime,
+                    msg,
+                ),
+            ),
+        )
+    return result
+
+
+async def downloader(filename, file, event, taime, msg):
+    with open(filename, "wb") as fk:
+        result = await downloadable(
+            client=event.client,
+            location=file,
+            out=fk,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d,
+                    t,
+                    event,
+                    taime,
+                    msg,
+                ),
+            ),
+        )
+    return result
+#______________________ Progress/Status Bar ______________________#
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (
+        ((str(weeks) + "w:") if weeks else "")
+        + ((str(days) + "d:") if days else "")
+        + ((str(hours) + "h:") if hours else "")
+        + ((str(minutes) + "m:") if minutes else "")
+        + ((str(seconds) + "s") if seconds else "")
+    )
+    if tmp != "":
+        if tmp.endswith(":"):
+            return tmp[:-1]
+        else:
+            return tmp
+    else:
+        return "0 s"
+
+
+def humanbytes(size):
+    if size in [None, ""]:
+        return "0 B"
+    for unit in ["B", "KB", "MB", "GB"]:
+        if size < 1024:
+            break
+        size /= 1024
+    return f"{size:.2f} {unit}"
+
+
+def numerize(number):
+    for unit in ["", "K", "M", "B", "T"]:
+        if number < 1000:
+            break
+        number /= 1000
+    return f"{number:.2f} {unit}"
+
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    diff = time.time() - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        time_to_completion = round((total - current) / speed) * 1000
+        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+            "".join("■" for i in range(math.floor(percentage / 5))),
+            "".join("" for i in range(20 - math.floor(percentage / 5))),
+            round(percentage, 2),
+        )
+
+        tmp = (
+            progress_str
+            + "`{0} of {1}`\n\n`• 🌠 Speed: {2}/s`\n\n`• ⏰ ETA: {3}`\n\n".format(
+                humanbytes(current),
+                humanbytes(total),
+                humanbytes(speed),
+                time_formatter(time_to_completion),
+            )
+        )
+        if file_name:
+            await event.edit(
+                "`• {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("`• {}`\n\n{}".format(type_of_ps, tmp))
+
+
+  
+  
+  
+  
+  
